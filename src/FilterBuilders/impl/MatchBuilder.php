79ae1d009ea418a0c9618a8bbdea9510f3bf5a1f
Phu Luong <phult.contact@gmail.com>
2021-01-29 11:22:40 +0700

    feature: fulltext filter

---
+++ src/FilterBuilders/impl/MatchBuilder.php
@@ -0,0 +1,26 @@
+<?php
+namespace Megaads\Apify\FilterBuilders\Impl;
+
+use Megaads\Apify\FilterBuilders\FilterBuilder;
+
+class MatchBuilder extends FilterBuilder
+{
+    const regex = '/(^[a-zA-Z0-9\.\_\-]+)\$(.*)/';
+    protected $level = 12;
+    public function buildQueryParam($filterParam)
+    {
+        preg_match(self::regex, $filterParam, $matches);
+        if (count($matches) == 3) {
+            return [
+                "field" => $matches[1],
+                "value" => $matches[2],
+            ];
+        } else {
+            return false;
+        }
+    }
+    public function buildQuery($query, $filter)
+    {
+        return $query->whereRaw("MATCH (" . $filter['field'] . ") AGAINST (" . $filter['value'] . " IN BOOLEAN MODE)");
+    }
+}
