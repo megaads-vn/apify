3b4b49a57fd2a6e6790215140eb1365107cc8a74
Bach Nguyen <bachnx2303@gmail.com>
2024-01-30 11:45:58 +0700

    optimize: Add Header Authentication for CORS

--- src/Middlewares/CorsMiddleware.php
+++ src/Middlewares/CorsMiddleware.php
@@ -11,7 +11,7 @@ class CorsMiddleware
             'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE, PATCH',
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Max-Age' => '86400',
-            'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With',
+            'Access-Control-Allow-Headers' => 'Content-Type, Authorization, Authentication, X-Requested-With',
         ];
 
         if ($request->isMethod('OPTIONS')) {
